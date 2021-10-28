bd0ff70b5b39b3e8a0b758a8e373a1242018be3b
swarajp688 <swarajp688@gmail.com>
2021-10-28 21:37:44 +0530

    added comment schema

--- controllers/posts_conntroller.js
+++ controllers/posts_conntroller.js
@@ -1,5 +1,6 @@
 const Post = require('../models/post')
-
+const Comment = require('../models/comment');
+const { post } = require('../routes/posts');
 module.exports.create =  function(req,res) {
     Post.create({
         content:req.body.content,
@@ -13,3 +14,12 @@ module.exports.create =  function(req,res) {
         return res.redirect('back');
     })
 }
+
+
+// module.exports.newComment = function(req,res) {
+//     Comment.create({
+//         content:req.body.content,
+//         user:user._id,
+//         post:post.id
+//     })
+// }
