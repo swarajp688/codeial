13ac2684ad9d81c640377d1f6df13542f66fb0c4
swarajp688 <swarajp688@gmail.com>
2021-12-07 12:23:50 +0530

    Added Profile Photo Option

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cookieParser =  require('cookie-parser');
 const app = express();
 const port=8000;
 const expressLayouts = require('express-ejs-layouts');
+const multer = require('multer');
 //mongodb
 const db =require('./config/mongoose');
 //used for session cookie
@@ -27,6 +28,8 @@ app.use(sassMiddleware({
 app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(express.static('./assets'));
+//make the upload path available to browser
+app.use('/uploads',express.static(__dirname+'/uploads'));
 app.use(expressLayouts);
 //extract styles and script from sub pages into layout
 app.set('layout extractStyles',true);
